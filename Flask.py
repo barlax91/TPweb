fac2b1cc0469b6ef923bdc610e0151828288fe5c
barlax91 <benoit91750@gmail.com>
2021-12-02 12:32:02 +0100

    1

--- Flask.py
+++ Flask.py
@@ -1,2 +1,11 @@
-from flask import Flask
+import streamlit as st
+from flask import Flask, app
+app = Flask('__name__')
 
+@app.route("/")
+def index():
+    return "hello world!"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
