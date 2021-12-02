2317fed446cd86040131bdd7c45982f7ab24372e
barlax91 <benoit91750@gmail.com>
2021-12-02 15:27:14 +0100

    2

--- Flask.py
+++ Flask.py
@@ -1,10 +1,24 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
 from flask import Flask, app
+
 app = Flask('__name__')
+
+df = pd.read_csv('vgsales.csv')
+
 
 @app.route("/")
 def index():
-    return "hello world!"
+
+    return "Crash course Flask"
+
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World '
 
 
 if __name__ == '__main__':
