edc8f4d1eb7a3b1447260b8f7d074f40f3209972
barlax91 <benoit91750@gmail.com>
2021-12-03 12:32:18 +0100

    upd

--- flask_api/FlaskApi.py
+++ flask_api/FlaskApi.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify,request, url_for
+from flask import Flask, render_template, jsonify, request, url_for
 import json
 
 from numpy import random
@@ -7,73 +7,53 @@ from numpy import random
 app = Flask(__name__)
 
 
+books = [
+        {
+            'id': 1,
+            'titre': 'un titre',
+        },
+        {
+            'id': 2,
+            'titre': 'un autre titre random',
+        }
+    ]
+books = json.load(open("books.json"))
+
+
 @app.route('/')
 def home():
-    return 'Hello my app'
+    return "Hello my app"
+
+
+@app.route('/ma_route/<name>', methods=["GET"])
+def get_name(name):
+    return "Hello"+name
 
 
 @app.route('/api/books', methods=['GET'])
 def index():
 
-    # cree un livre
-    book = [
-        {
-            'id': 1,
-            'titre': 'un titre',
-        },
-        {
-            'id': 2,
-            'titre': 'un autre titre random',
-        }
-    ]
-    book = json.dumps(book)
+    return jsonify(books)
+
+
+@app.route('/api/books/<int:id>', methods=['GET'])
+def getBook_id(id):
+    book = None
+    for val in books:
+        if val.get("isbn") == str(id):
+            book = val
+            break
     return jsonify(book)
 
 
-@app.route('/api/books/id', methods=['GET'])
-def getBook_id():
-
-    book = [
-        {
-            'id': 1,
-            'titre': 'un titre',
-
-        },
-        {
-            'id': 2,
-            'titre': 'un autre titre random',
-        }
-    ]
-
-    RandBook = random.randint(0, 1)
-    print(RandBook)
-    if book[RandBook]['id'] == 1:
-        return jsonify(book[RandBook])
-    if book[RandBook]['id'] == 2:
-        return jsonify(book[RandBook])
-
-
-@app.route('/api/books/title', methods=['GET'])
-def getBook_title():
-
-    book = [
-        {
-            'id': 1,
-            'titre': 'un titre',
-
-        },
-        {
-            'id': 2,
-            'titre': 'un autre titre random',
-        }
-    ]
-
-    RandBook = random.randint(0, 1)
-    print(RandBook)
-    if book[RandBook]['title'] == 1:
-        return jsonify(book[RandBook])
-    if book[RandBook]['title'] == 2:
-        return jsonify(book[RandBook])
+@app.route('/api/books/<string:title>', methods=['GET'])
+def getBook_title(title):
+    book = None
+    for val in books:
+        if val.get("title") == title:
+            book = val
+            break
+    return jsonify(book)
 
 
 @app.route('/api/books/catalogue', methods=["POST", "GET"])
